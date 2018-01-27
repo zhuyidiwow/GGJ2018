ad35df8ed97fd7ca780e1d2d9e6cf408bda31ff4
Yidi-Z <zhuyidiwow@gmail.com>
2018-01-27 10:43:10 -0500

    Pointer input and InputManager settings

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,8 +10,21 @@ public class Player : MonoBehaviour {
 	public EPlayer EPlayer;
 	public int Score = 0;
 	public bool ShouldReceiveInput = true;
+	public float InputThreshold = 0.1f;
 		
 	public void GetScore() {
 			
+	}
+
+	public int GetPlayerNo() {
+		switch (EPlayer) {
+			case EPlayer.Left:
+				return 1;
+			case EPlayer.Right:
+				return 2;
+			default:
+				Debug.LogError("Eplayer error");
+				return 0;
+		}
 	}
 }
