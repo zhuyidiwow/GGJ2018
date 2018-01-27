ef1768ce66ad9a8d8bebfbf2e1b6dc4d8ea72701
Yidi-Z <zhuyidiwow@gmail.com>
2018-01-27 11:57:00 -0500

    InputManager Configurations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,28 +11,6 @@ public class Player : MonoBehaviour {
 	public int Score = 0;
 	public bool ShouldReceiveInput = true;
 	public float InputThreshold = 0.5f;
-	public GameObject Wheel;
-
-	private float currentAngle;
-	private float lastFrameAngle; // counterclockwise from x axis, in deg
-	private string horiAxis;
-	private string verAxis;
-
-	private void Start() {
-		horiAxis = "Wheel" + GetPlayerNo() + "Hori";
-		verAxis = "Wheel" + GetPlayerNo() + "Ver";
-	}
-
-	private void Update() {
-		float x = Input.GetAxis(horiAxis);
-		float y = Input.GetAxis(verAxis);
-		if (Utilities.Math.GetMagnitude(new Vector2(x, y)) > InputThreshold) {
-			currentAngle = Utilities.Math.GetAngle(x, y);
-			float dAngle = currentAngle - lastFrameAngle; // counterclock wise, in deg
-			transform.Rotate(0f, 0f, dAngle);
-			lastFrameAngle = currentAngle;
-		}
-	}
 
 	public void GetScore() {
 			
---
+++ Assets/Wheel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour {
+	
+	private Player player;
+	
+	private float currentAngle;
+	private float lastFrameAngle; // counterclockwise from x axis, in deg
+	private string horiAxis;
+	private string verAxis;
+
+	private void Start() {
+		player = transform.parent.GetComponent<Player>();
+		horiAxis = "Wheel" + player.GetPlayerNo() + "Hori";
+		verAxis = "Wheel" + player.GetPlayerNo() + "Ver";
+	}
+
+	private void Update() {
+		float x = Input.GetAxis(horiAxis);
+		float y = Input.GetAxis(verAxis);
+		if (Utilities.Math.GetMagnitude(new Vector2(x, y)) > player.InputThreshold) {
+			currentAngle = Utilities.Math.GetAngle(x, y);
+			float dAngle = currentAngle - lastFrameAngle; // counterclock wise, in deg
+			transform.Rotate(0f, 0f, dAngle);
+			lastFrameAngle = currentAngle;
+		}
+	}
+}
