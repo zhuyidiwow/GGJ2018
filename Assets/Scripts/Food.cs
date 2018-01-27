ce44a08ca4eeb1f1f9177abb85d286365f5a338e
Lotusly <yul4@andrew.cmu.edu>
2018-01-27 11:08:59 -0500

    scene and food generator, foods

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+
+	public int id;
+	public float error;
+	public Transform destination;
+
+	public float movingSpeed;
+
+	public float rotatingSpeed;
+
+	public int state;
+	
+	//-------------TEST-------------
+
+	public Vector3 trans;
+	// Use this for initialization
+	void Start ()
+	{
+		//state = 0;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		switch (state){
+			case 1:
+			{
+				if (Vector3.Distance(transform.position, destination.position) < error)
+				{
+					state = 2;
+					break;
+				}
+				else
+				{
+					trans = (destination.position - transform.position).normalized * movingSpeed * Time.deltaTime;
+					transform.Translate(trans,Space.World);
+					transform.Rotate(transform.forward,rotatingSpeed,Space.World);
+				}
+				break;
+			}
+			case 2:
+			{
+				Destroy(gameObject);
+				break;
+			}
+			
+		}
+	}
+
+	public void Initialize(Transform des,float mSpeed,float rSpeed)
+	{
+		destination = des;
+		movingSpeed = mSpeed;
+		rotatingSpeed = rSpeed;
+		state = 1;
+	}
+}
--- Assets/Scripts/FoodGenerator.cs
+++ Assets/Scripts/FoodGenerator.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class FoodGenerator : MonoBehaviour
 {
 
-	[SerializeField] private GameObject[] foods;
+	public GameObject[] foods;
+	public float AmountPerSecond;
+	public Transform FoodContainer;
+	public Transform Wheel;
+	
+	public float initialSpeed;
+	public float rotationSpeed;
+
+	public float edge;
+	public AnimationCurve PositionDistribution;
+	
+	
+	//-----------TEST---------------
+	public 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Random.value/Time.deltaTime<AmountPerSecond)
+		{
+			Generate(Random.Range(0,foods.Length),
+				transform.position+transform.up*PositionDistribution.Evaluate(Random.value)*edge*2+new Vector3(0,-edge,-0.1f));
+				//transform.position+new Vector3(0,PositionDistribution.Evaluate(Random.value)*edge*2-edge,-0.1f));
+			
+		}
 	}
 
-	void Generate(int id, double place)
+	void Generate(int id, Vector3 place)
 	{
 		if (id >= 0 && id < foods.Length)
 		{
-			GameObject food = foods[id];
-			
+			GameObject food = Instantiate(foods[id]);
+			food.transform.position = place;
+			food.transform.parent = FoodContainer;
+			food.GetComponent<Food>().Initialize(Wheel,initialSpeed,rotationSpeed);
 		}
 	}
 }
